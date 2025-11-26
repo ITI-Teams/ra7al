d23222f8d6145e7a179ea871987dca3e6fe59715
AbdullahShokr1 <darkthe254@gmail.com>
2025-11-26 11:15:46 +0200

    Dashboard

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,7 +1,39 @@
-import './bootstrap';
-
-import Alpine from 'alpinejs';
+import Alpine from "alpinejs";
 
 window.Alpine = Alpine;
+
+window.themeData = function () {
+    return {
+        dark: false,
+        open: true,
+
+        init() {
+            const saved = localStorage.getItem('theme');
+            const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+            this.dark = saved ? saved === 'dark' : systemDark;
+
+            this.applyTheme();
+        },
+
+        toggleDarkMode() {
+            this.dark = !this.dark;
+            if(this.dark){
+                document.body.classList.add('dark');
+            } else {
+                document.body.classList.remove('dark');
+            }
+            localStorage.setItem('theme', this.dark ? 'dark' : 'light');
+            this.applyTheme();
+        },
+
+        applyTheme() {
+            if (this.dark) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+        }
+    }
+}
 
 Alpine.start();
