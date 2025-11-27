6d9da9af766d27a4ce7cc86a1fd5ea2d723094bc
saadsafwat4444 <saadsafwat2522@gmail.com>
2025-11-27 02:29:46 +0200

    About Us

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { AboutUs } from './components/about-us/about-us';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+   {
+    path: 'about',
+    component: AboutUs,
   }
 ];
