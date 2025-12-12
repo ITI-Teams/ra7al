a241efc92428f9494cc04a98c0025e52195a4cea
saadsafwat4444 <saadsafwat2522@gmail.com>
2025-12-12 03:51:47 +0200

    pull into main

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api',
-  imageUrl: 'http://localhost:8000',
+  apiUrl: 'http://localhost:8010/api',
+  imageUrl: 'http://localhost:8010',
   pusherKey: '1d3964aed5b0925ac685',
   pusherCluster: 'eu',
 };
